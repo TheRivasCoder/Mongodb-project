404a75cec43464cfc9d6361411265a25f59c75fe
Chris Rivas <chrisrivasstudio@gmail.com>
2021-03-18 23:22:34 -0700

    Add to scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -10,10 +10,10 @@ $("form[name=signup_form").submit(function(e) {
     data: data,
     dataType: "json",
     success: function(resp) {
-      console.log(resp);
+      window.location.href = "/dashboard/";
     },
     error: function(resp) {
-      console.log(resp);
+      $error.text(resp.responseJSON.error).removeClass("error--hidden");
     }
   });
   
